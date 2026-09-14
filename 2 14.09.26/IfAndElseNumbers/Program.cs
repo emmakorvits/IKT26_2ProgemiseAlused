5df14396a72c21dcbfbd3dd1f6f98a725d3e45b7
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-14 11:05:35 +0300

    fix

--- 2 14.09.26/IfAndElseNumbers/Program.cs
+++ 2 14.09.26/IfAndElseNumbers/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Kirjuta enda vanus");
 
             //peate kasutama if and else lauseid,
             //et kontrollida, kas kasutaja vanus
