a98d9b13cd0b1e46401a9bc54648dd593b87e2a8
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-14 11:01:17 +0300

    IfAndElseNumbers

---
+++ 2 14.09.26/IfAndElseNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace IfAndElseNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //peate kasutama if and else lauseid,
+            //et kontrollida, kas kasutaja vanus
+            //on suurem kui 18 või väiksem kui 18
+
+            //saab kasutada Converti ja Parse-t
+            //int userAge = Convert.ToInt32(Console.ReadLine());
+            string input = Console.ReadLine();
+            int age = int.Parse(input);
+
+            if(age > 17)
+            { Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("täisealine");
+
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("alakas");
+
+            }
+        }
+    }
+}
