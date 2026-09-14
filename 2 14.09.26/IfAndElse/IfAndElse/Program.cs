5151e1ddfa01ab9d01cb2b0e3aa3ec61685023f0
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-14 09:58:09 +0300

    IfAndElse

---
+++ 2 14.09.26/IfAndElse/IfAndElse/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace IfAndElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta enda nimi");
+
+            //siin on muutuja nimega name
+            //mis on tüübiga string
+            //loeb andmeid konsoolist ja salvestab
+            //need muutuja nime sisse
+            string name = Console.ReadLine();
+
+            if (name != "")
+            {
+                Console.WriteLine("Tere, " + name);
+            }
+            else
+            {
+                Console.WriteLine("tere, tundmatu!");
+            }
+        }
+    }
+}
